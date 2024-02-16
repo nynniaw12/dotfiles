d46544d043b363043c8a16be938aa297a62474bc
Hasan Aybars ARI <hasanaybarsari@dhcp-10-105-235-178.wireless.northwestern.private>
2024-02-15 19:46:34 -0600

    snippets and autocomplete fixed

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,20 +1,37 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'texlab', 'eslint', 'tsserver', 'rust_analyzer'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  },
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+local cmp_format = require('lsp-zero').cmp_format()
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    })
 })
 
 
-
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = { "lua_ls", 'texlab', 'eslint', 'tsserver', 'rust_analyzer' },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    },
+})
--- nvim/lua/aybo/packer.lua
+++ nvim/lua/aybo/packer.lua
@@ -1,58 +1,62 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5', -- or branch = '0.1.x',
-		requires = { 'nvim-lua/plenary.nvim' }
-	}
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5', -- or branch = '0.1.x',
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
 
-	use {
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	}
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-			ts_update()
-		end,}
-		use("nvim-treesitter/playground")
+    use {
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end, }
+    use("nvim-treesitter/playground")
 
-		use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-		use {
-			"ThePrimeagen/harpoon",
-			branch = "harpoon2",
-			requires = { {"nvim-lua/plenary.nvim"} }
-		}
-		use ("mbbill/undotree")
-		use ("tpope/vim-fugitive")
-		use {
-			'VonHeikemen/lsp-zero.nvim',
-			branch = 'v3.x',
-			requires = {
-				--- Uncomment the two plugins below if you want to manage the language servers from neovim
+    use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
 
-				-- LSP Support
-				{'neovim/nvim-lspconfig'},
-				-- Autocompletion
-				{'hrsh7th/nvim-cmp'},
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'L3MON4D3/LuaSnip'},
-			}
-		}
-		use {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-		}
-        use 'lervag/vimtex'
-	end)
-
-
-
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+    use 'lervag/vimtex'
+    use({
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!:).
+        run = "make install_jsregexp"
+    })
+end)
