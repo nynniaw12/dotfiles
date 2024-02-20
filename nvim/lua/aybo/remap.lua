4608aea43c73c12f258b55e3ccfdb5cc9d5decb8
nynniaw12 <aybars.ari@icloud.com>
2024-02-20 16:39:25 -0600

    macos defaults

--- nvim/lua/aybo/remap.lua
+++ nvim/lua/aybo/remap.lua
@@ -41,8 +41,8 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
-vim.keymap.set( "n" , "<leader>bn,", "<cmd>bnext<CR>")
-vim.keymap.set( "n" , "<leader>bp" ,"<cmd>bprevious<CR>")
-vim.keymap.set( "n" , "<leader>bd", "<cmd>bdelete<CR>")
 
+vim.keymap.set( "n" , "<leader>bn", "<cmd>bnext<CR>", { noremap = true })
+vim.keymap.set( "n" , "<leader>bp" ,"<cmd>bprevious<CR>", { noremap = true })
+vim.keymap.set( "n" , "<leader>bd", "<cmd>bdelete<CR>", { noremap = true })
 
